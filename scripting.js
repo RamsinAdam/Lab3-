31551d79577674a7b94b16d84759365a6b18ea74
RamsinAdam <133426494+RamsinAdam@users.noreply.github.com>
2024-11-12 18:21:31 -0500

    functions that contain data of the car

--- scripting.js
+++ scripting.js
@@ -1,3 +1,20 @@
 // Toy Car Constructor
 // This constructor function is used to create new ToyCar objects with specific properties
 function ToyCar(model, brand, color, price, length, width, height, weight, material, year, scale, vehicleType, sku, upc, packaging) {
+    this.model = model;
+    this.brand = brand;
+    this.color = color;
+    this.price = price;
+    this.dimensions = { 
+        length: length,
+        width: width,
+        height: height
+    };
+    this.weight = weight;
+    this.material = material;
+    this.year = year;
+    this.scale = scale;
+    this.vehicleType = vehicleType;
+    this.sku = sku;
+    this.upc = upc;
+    this.packaging = packaging;
