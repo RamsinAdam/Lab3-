10894e1fffb6fff6ea9ea526945bd7fdf7ef3822
RamsinAdam <133426494+RamsinAdam@users.noreply.github.com>
2024-11-12 18:20:55 -0500

    added toy car constructor

---
+++ scripting.js
@@ -0,0 +1,3 @@
+// Toy Car Constructor
+// This constructor function is used to create new ToyCar objects with specific properties
+function ToyCar(model, brand, color, price, length, width, height, weight, material, year, scale, vehicleType, sku, upc, packaging) {
